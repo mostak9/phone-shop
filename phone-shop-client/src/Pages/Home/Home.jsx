cf25cd1d4f5795bd4c7afe26b8d29682df442571
mostak9 <mostak.ahm263@gmail.com>
2023-12-20 22:10:41 +0600

    construct basic structure

---
+++ phone-shop-client/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div className="text-5xl">
+            This is homepage.
+            <button className="btn btn-primary">Button</button>
+        </div>
+    );
+};
+
+export default Home;

264f57ad1759a12c1376a239b5803b6855adae14
mostak9 <mostak.ahm263@gmail.com>
2023-12-21 22:00:25 +0600

    added api to get sigle product

--- phone-shop-server/index.js
+++ phone-shop-server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -32,9 +32,16 @@ async function run() {
 
 
 
-    app.get('/phones', async(req, res) => {
-        const result = await phonesCollection.aggregate([{$sample: {size: 10}}]).toArray();
+    app.get('/latestProducts', async(req, res) => {
+        const result = await phonesCollection.aggregate([{$sample: {size: 12}}]).toArray();
         res.send(result);
+    })
+
+    app.get('/productDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const product = await phonesCollection.findOne(query);
+      res.send(product);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
